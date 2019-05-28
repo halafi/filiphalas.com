50fda18f5948485a5b257142725c9d53f89cc3d9
halafi <filiphalas74@gmail.com>
2019-05-28 12:50:06 +0200

    feat(title):

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Filip Halas - JS Developer',
+    title: 'Filip Halas - Full-Stack Web Developer (React, Node, SEO)',
     siteUrl: 'https://filiphalas.com',
   },
   plugins: [
