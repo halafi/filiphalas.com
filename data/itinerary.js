1d0d8f620f3afc14a5074a52a3fb31e32db15ee2
halafi <filiphalas74@gmail.com>
2023-04-14 12:49:06 +0900

    chore: update

--- data/itinerary.js
+++ data/itinerary.js
@@ -1,32 +1,32 @@
 const itinerary = [
-  {
-    origin: "Bangkok DMK",
-    destination: "Phnom Penh PNH",
-    date: "2023-01-28 15:15:00.000",
-    flightNumber: "FD 606",
-    aeroline: 'kiwi',
-  },
-  {
-    origin: "Phnom Penh PNH",
-    destination: "Bangkok DMK",
-    date: "2023-02-04 17:15:00.000",
-    flightNumber: "FD 607",
-    aeroline: 'kiwi',
-  },
-  {
-    origin: "Bangkok DMK",
-    destination: "Taipei TPE",
-    date: "2023-03-20 06:40:00.000",
-    flightNumber: "FD 230",
-    aeroline: 'kiwi',
-  },
-  {
-    origin: "Taipei TPE",
-    destination: "Okinawa Island OKA",
-    date: "2023-03-28 14:50:00.000",
-    flightNumber: "MM 926",
-    aeroline: 'kiwi',
-  },
+  // {
+  //   origin: "Bangkok DMK",
+  //   destination: "Phnom Penh PNH",
+  //   date: "2023-01-28 15:15:00.000",
+  //   flightNumber: "FD 606",
+  //   aeroline: 'kiwi',
+  // },
+  // {
+  //   origin: "Phnom Penh PNH",
+  //   destination: "Bangkok DMK",
+  //   date: "2023-02-04 17:15:00.000",
+  //   flightNumber: "FD 607",
+  //   aeroline: 'kiwi',
+  // },
+  // {
+  //   origin: "Bangkok DMK",
+  //   destination: "Taipei TPE",
+  //   date: "2023-03-20 06:40:00.000",
+  //   flightNumber: "FD 230",
+  //   aeroline: 'kiwi',
+  // },
+  // {
+  //   origin: "Taipei TPE",
+  //   destination: "Okinawa Island OKA",
+  //   date: "2023-03-28 14:50:00.000",
+  //   flightNumber: "MM 926",
+  //   aeroline: 'kiwi',
+  // },
   {
     origin: "Okinawa Island OKA",
     destination: "Osaka (KIX)",
@@ -40,6 +40,13 @@ const itinerary = [
     date: "2023-05-12 08:15:00.000",
     flightNumber: "FD 237",
     aeroline: 'kiwi',
+  },
+  {
+    origin: "Bangkok Suvanabhumi BKK",
+    destination: "Seoul ICN",
+    date: "2023-06-09 03:00:00.000",
+    flightNumber: "XJ 700",
+    aeroline: 'airasia',
   },
 ];
 
